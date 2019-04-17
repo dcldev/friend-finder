6fa0dc997a1e722c39b26fac0e40fe5c101db789
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-17 15:35:33 -0400

    added friends.js file with seinfeld charcters and initialized the script file for the survey to correlate with the seinfeld characters and survey answers.

---
+++ app/public/js/scripts.js
@@ -0,0 +1,8 @@
+let file = "";
+let fullName = "";
+let userAnswers = []; // Contains the user input answers for the survey
+let currentSurvey = 0; // Records the survey questions
+
+let survey = [{
+    
+}]
